58a1539f3b8b05cf7d017e8267dc9abc60f6dee1
JcKay <kay.jangchul@gmail.com>
2021-11-10 17:41:33 +0630

    oop coffee machine complete

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,23 @@
-from menu import Menu, MenuItem
+# MODULES
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from menu import Menu, MenuItem
+
+# OBJECTS LIST
+cMaker = CoffeeMaker()
+mMachine = MoneyMachine()
+cMenu = Menu()
+
+while True:
+    options = cMenu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
+        break
+    elif choice == "report":
+        cMaker.report()
+        mMachine.report()
+    else:
+        # check resource / check payment / make coffee
+        drink = cMenu.find_drink(choice)
+        if cMaker.is_resource_sufficient(drink) and mMachine.make_payment(drink.cost):
+            cMaker.make_coffee(drink)
